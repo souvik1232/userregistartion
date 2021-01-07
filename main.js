126ae258957219a74960f58570095ed082cfc114
souvik1232 <souvik199831@gmail.com>
2021-01-07 12:51:59 +0530

    Added:Function to check valid first name input

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
 const utility=require('./utility')
-const readline = require('readline-sync')
 
-let firstName = readline.question('Enter First Name ');
-
-const retData=utility.printName(firstName);
-console.log("it is in main.js",retData);
+utility.enterValidFirstName();
--- utility.js
+++ utility.js
@@ -1,8 +1,11 @@
+const readline = require('readline-sync')
 class Utility {
-    printName=(name) =>{
-        console.log(" name ", name);
-
-        return "my name is ",name;
+    enterValidFirstName=() =>{
+        let firstName = readline.question('Enter First Name: ');
+        do { 
+            firstName = readline.question("Enter valid First name: ")
+        } while (!firstName.match("^[A-Z]{1}[a-z]{2,}$"));
+        console.log("Valid");
     }
 }
 module.exports = new Utility();
